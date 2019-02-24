2fb534e5430414ce2fe080643419d0a8878bdc1c
angeloablaza <40690315+angeloablaza@users.noreply.github.com>
2019-02-24 10:04:29 +0800

    random

--- Habify-Android/app/src/main/java/ph/edu/dlsu/ian_ona/habify/DashboardActivity.java
+++ Habify-Android/app/src/main/java/ph/edu/dlsu/ian_ona/habify/DashboardActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -14,7 +15,9 @@ public class DashboardActivity extends AppCompatActivity {
     private Adapter adapter;
     private RecyclerView.LayoutManager manager;
     private ArrayList<Model> tasks;
-    private ArrayList<Model> temp;
+    private ArrayList<Model> tempMind;
+    private ArrayList<Model> tempSoul;
+    private ArrayList<Model> tempPhysical;
     private Model task;
 
     @Override
@@ -30,71 +33,99 @@ public class DashboardActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
 
         tasks = new ArrayList<>();
-        temp = new ArrayList<>();
-        task = new Model("S", "Single", "02/27/2019");
+        tempMind = new ArrayList<>();
+        tempSoul = new ArrayList<>();
+        tempPhysical = new ArrayList<>();
     }
 
     public void generateTasks(View view){
-        populateMind();
+        populateMind(5);
         adapter.refreshTasks(tasks);
     }
 
     public void generateOne(View view){
+        Random rand = new Random();
+        int randNum = rand.nextInt((5 - 0) + 1) + 0;
+        int choice = rand.nextInt((3 - 1) + 1) + 1;
+        switch (choice){
+            case 1:
+                task = tempMind.get(randNum);
+                break;
+            case 2:
+                task = tempSoul.get(randNum);
+                break;
+            case 3:
+                task = tempPhysical.get(randNum);
+                break;
+        }
+//        task = new Model(temp);
         adapter.refreshOne(task);
     }
 
-    public void populateMind(){
+    public void populateMind(int limit){
         Model t1 = new Model("M", "Read 20 pages of a book you want to read", "02/28/2019");
         Model t2 = new Model("M", "Do some advanced studying for one lesson in one of your classes", "02/28/2019");
         Model t3 = new Model("M", "Read an article about something your interested in", "02/28/2019");
         Model t4 = new Model("M", "Memorize a fact on something you're interested in", "02/28/2019");
         Model t5 = new Model("M", "Review 1 lesson in one of your classes", "02/28/2019");
 
-        temp.add(t1);
-        temp.add(t2);
-        temp.add(t3);
-        temp.add(t4);
-        temp.add(t5);
+        tempMind.add(t1);
+        tempMind.add(t2);
+        tempMind.add(t3);
+        tempMind.add(t4);
+        tempMind.add(t5);
 
-        tasks.add(t1);
-        tasks.add(t2);
-        tasks.add(t3);
+//        tasks.add(t1);
+//        tasks.add(t2);
+//        tasks.add(t3);
+
+        for(int i = 0; i < limit; i++){
+            tasks.add(tempMind.get(i));
+        }
 
     }
 
-    public void populateSoul(){
+    public void populateSoul(int limit){
         Model s1 = new Model("S", "Meditate for 5 minutes", "02/28/2019");
         Model s2 = new Model("S", "List down the things you are thankful dfr", "02/28/2019");
         Model s3 = new Model("S", "Give someone a hug", "02/28/2019");
         Model s4 = new Model("S", "Make someone smile today", "02/28/2019");
         Model s5 = new Model("S", "Hang out with a friend today", "02/28/2019");
 
-        temp.add(s1);
-        temp.add(s2);
-        temp.add(s3);
-        temp.add(s4);
-        temp.add(s5);
+        tempSoul.add(s1);
+        tempSoul.add(s2);
+        tempSoul.add(s3);
+        tempSoul.add(s4);
+        tempSoul.add(s5);
 
-        tasks.add(s1);
-        tasks.add(s2);
-        tasks.add(s3);
+//        tasks.add(s1);
+//        tasks.add(s2);
+//        tasks.add(s3);
+
+        for(int i = 0; i < limit; i++){
+            tasks.add(tempMind.get(i));
+        }
     }
 
-    public void populatePhysical(){
+    public void populatePhysical(int limit){
         Model p1 = new Model("P", "Do 30 Sit-ups", "02/28/2019");
         Model p2 = new Model("P", "Take a 30 Minute Jog", "02/28/2019");
         Model p3 = new Model("P", "Do 50 Jumping Jacks", "02/28/2019");
         Model p4 = new Model("P", "Do 30 Push Ups", "02/28/2019");
         Model p5 = new Model("P", "Do 50 30 Squats", "02/28/2019");
 
-        temp.add(p1);
-        temp.add(p2);
-        temp.add(p3);
-        temp.add(p4);
-        temp.add(p5);
+        tempPhysical.add(p1);
+        tempPhysical.add(p2);
+        tempPhysical.add(p3);
+        tempPhysical.add(p4);
+        tempPhysical.add(p5);
 
-        tasks.add(p1);
-        tasks.add(p2);
-        tasks.add(p3);
+//        tasks.add(p1);
+//        tasks.add(p2);
+//        tasks.add(p3);
+
+        for(int i = 0; i < limit; i++){
+            tasks.add(tempPhysical.get(i));
+        }
     }
 }

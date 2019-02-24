a9c0fffb94d8c834c1a91d87aebcc829e9d5036c
angeloablaza <40690315+angeloablaza@users.noreply.github.com>
2019-02-24 11:45:10 +0800

    generate one fix

--- Habify-Android/app/src/main/java/ph/edu/dlsu/ian_ona/habify/DashboardActivity.java
+++ Habify-Android/app/src/main/java/ph/edu/dlsu/ian_ona/habify/DashboardActivity.java
@@ -16,6 +16,7 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -26,7 +27,7 @@ public class DashboardActivity extends AppCompatActivity {
     private ArrayList<Model> tempMind;
     private ArrayList<Model> tempSoul;
     private ArrayList<Model> tempPhysical;
-    private Model task;
+//    private Model task;
     private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,10 +61,11 @@ public class DashboardActivity extends AppCompatActivity {
     }
 
     public void generateNow(){
-        tempMind.clear();
-        tempPhysical.clear();
-        tempSoul.clear();
-        tasks.clear();
+        tempMind = new ArrayList<>();
+        tempPhysical = new ArrayList<>();
+        tempSoul = new ArrayList<>();
+        tasks = new ArrayList<>();
+
         SharedPreferences sharedPref = getSharedPreferences(getString(R.string.preferences), Context.MODE_PRIVATE);
 
         int userScore = sharedPref.getInt(getString(R.string.userScorePref), 0);
@@ -89,24 +91,23 @@ public class DashboardActivity extends AppCompatActivity {
     }
 
     public void generateOne(View view){
-        populateSoul(5);
-        populatePhysical(5);
-        populateMind(5);
-        Random rand = new Random();
-        int randNum = rand.nextInt((4 - 0) + 1) + 0;
-        int choice = rand.nextInt((3 - 1) + 1) + 1;
+        populateAll();
+        Model task = null;
+        int randomNum = ThreadLocalRandom.current().nextInt(0, 4 + 1);
+        int choice = ThreadLocalRandom.current().nextInt(1, 3 + 1);
         switch (choice){
             case 1:
-                task = tempMind.get(randNum);
+                task = tempMind.get(randomNum);
                 break;
             case 2:
-                task = tempSoul.get(randNum);
+                task = tempSoul.get(randomNum);
                 break;
             case 3:
-                task = tempPhysical.get(randNum);
+                task = tempPhysical.get(randomNum);
                 break;
         }
 //        task = new Model(temp);
+//        Model task = new Model("M", "Single", "02/24/2019");
         adapter.refreshOne(task);
     }
 
@@ -137,7 +138,7 @@ public class DashboardActivity extends AppCompatActivity {
 
     public void populateSoul(int limit){
         Model s1 = new Model("S", "Meditate for 5 minutes", "02/28/2019");
-        Model s2 = new Model("S", "List down the things you are thankful dfr", "02/28/2019");
+        Model s2 = new Model("S", "List down the things you are thankful for", "02/28/2019");
         Model s3 = new Model("S", "Give someone a hug", "02/28/2019");
         Model s4 = new Model("S", "Make someone smile today", "02/28/2019");
         Model s5 = new Model("S", "Hang out with a friend today", "02/28/2019");
@@ -180,6 +181,44 @@ public class DashboardActivity extends AppCompatActivity {
             Log.d("ADDING BODY", "here" +tempPhysical.get(i));
 
         }
+    }
+
+    public void populateAll(){
+        Model t1 = new Model("M", "Read 20 pages of a book you want to read", "02/28/2019");
+        Model t2 = new Model("M", "Do some advanced studying for one lesson in one of your classes", "02/28/2019");
+        Model t3 = new Model("M", "Read an article about something your interested in", "02/28/2019");
+        Model t4 = new Model("M", "Memorize a fact on something you're interested in", "02/28/2019");
+        Model t5 = new Model("M", "Review 1 lesson in one of your classes", "02/28/2019");
+
+        tempMind.add(t1);
+        tempMind.add(t2);
+        tempMind.add(t3);
+        tempMind.add(t4);
+        tempMind.add(t5);
+
+        Model p1 = new Model("P", "Do 30 Sit-ups", "02/28/2019");
+        Model p2 = new Model("P", "Take a 30 Minute Jog", "02/28/2019");
+        Model p3 = new Model("P", "Do 50 Jumping Jacks", "02/28/2019");
+        Model p4 = new Model("P", "Do 30 Push Ups", "02/28/2019");
+        Model p5 = new Model("P", "Do 50 30 Squats", "02/28/2019");
+
+        tempPhysical.add(p1);
+        tempPhysical.add(p2);
+        tempPhysical.add(p3);
+        tempPhysical.add(p4);
+        tempPhysical.add(p5);
+
+        Model s1 = new Model("S", "Meditate for 5 minutes", "02/28/2019");
+        Model s2 = new Model("S", "List down the things you are thankful for", "02/28/2019");
+        Model s3 = new Model("S", "Give someone a hug", "02/28/2019");
+        Model s4 = new Model("S", "Make someone smile today", "02/28/2019");
+        Model s5 = new Model("S", "Hang out with a friend today", "02/28/2019");
+
+        tempSoul.add(s1);
+        tempSoul.add(s2);
+        tempSoul.add(s3);
+        tempSoul.add(s4);
+        tempSoul.add(s5);
     }
 
     public void viewProgress(View view){
